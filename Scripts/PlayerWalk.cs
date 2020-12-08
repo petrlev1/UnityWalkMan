a672d50d579e8a0a99c5ceab6f56d4d1b5311ef2
petrlev1 <levendeevp@gmail.com>
2020-12-09 00:30:24 +0300

    Добавлено управление игрока - движение и прыжок

---
+++ Scripts/PlayerWalk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    
+	//movement speed in units per second
+    public float movementSpeed = 5f;
+
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+		float horizontalInput2 = Input.GetAxis("Mouse X");
+        //get the Input from Vertical axis
+		float verticalInput = Input.GetAxis("Vertical");
+        float verticalInput2 = Input.GetAxis("Mouse Y");
+
+        //update the position
+        transform.localPosition = transform.localPosition + new Vector3( horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime );
+		 transform.localPosition = transform.localPosition + new Vector3( horizontalInput2 * movementSpeed * Time.deltaTime, 0, verticalInput2 * movementSpeed * Time.deltaTime );
+
+        //output to log the position change
+        //Debug.Log(transform.position);
+    }
+	
+}
