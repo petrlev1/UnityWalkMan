3886a57c573203f56a01697cf0a1e19238a085f4
petrlev1 <levendeevp@gmail.com>
2021-01-21 23:28:31 +0300

    Доработано управление игроком и оружием при помощи мыши

---
+++ Scripts/GunControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+	
+    public GameObject GunTarg1;
+	public GameObject GunTarg2;
+	
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		
+		//Debug.Log ( "Gun: " + this.transform.rotation.x );
+		
+		this.transform.rotation = Quaternion.Euler( GunTarg1.transform.localRotation.x * 100, transform.eulerAngles.y, transform.eulerAngles.z );
+		
+		//this.transform.rotation = Quaternion.Euler(GunTarg1.transform.rotation.x * 100, this.transform.rotation.y, 0);
+		//this.transform.rotation.x = Quaternion.Euler(GunTarg1.transform.rotation.x);
+		
+		//this.transform.rotation.x = TargetObj.transform.rotation.x;
+		
+		//TargetObj.transform.localRotation;
+        
+    }
+}
---
+++ Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+	public GameObject PlayerTargetObj;
+	
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		this.transform.rotation = Quaternion.Euler( transform.eulerAngles.x, PlayerTargetObj.transform.eulerAngles.y, transform.eulerAngles.z );
+		
+		//Debug.Log ( PlayerTargetObj.transform.rotation );
+		//Debug.Log ( PlayerTargetObj.transform.eulerAngles.y );
+        
+    }
+}
